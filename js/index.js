f94fb04428c16ee2161ef1f2d32dde8a80399e44
ScienceMind <sciensemind@gmail.com>
2024-09-02 15:14:47 +0300

    js part started

---
+++ js/index.js
@@ -0,0 +1,31 @@
+import variables from "./variables.js";
+import state from "./state.js";
+
+const { selects, success } = variables
+
+const renderCodeList = () => {
+    selects.forEach((select) => {
+        state.codes.forEach(([code]) => {
+            const element = document.createElement('option');
+            element.value = code;
+            element.innerText = code;
+            select.insertAdjacentElement('beforeend', element);
+        });
+    });
+};
+
+const fetchCodes = async () => {
+    try {
+        const response = await fetch(`${state.url}/codes`);
+        const data = await response.json();
+
+        if (data.result === success) {
+            state.codes = data.supported_codes
+            renderCodeList();
+        }
+    } catch (err) {
+        console.log(err)
+    }
+};
+
+fetchCodes();
---
+++ js/state.js
@@ -0,0 +1,4 @@
+export default {
+    url: "https://v6.exchangerate-api.com/v6/f8b390373f5eea7838ab6f92",
+    codes: [],
+}
